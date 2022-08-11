37a1c00af67a068d9464a6d49f69129e22276fb0
eli <qq1358722950@gmail.com>
2022-08-11 13:57:50 +0800

    chore: add `lsp` and `rome` face

--- mindre-theme.el
+++ mindre-theme.el
@@ -890,9 +890,16 @@ Takes care of adding or removing hooks when the
     `(geiser-font-lock-autodoc-current-arg ((t :inherit mindre-verbatim)))
     `(geiser-font-lock-autodoc-identifier ((t :inherit mindre-keyword)))
 
-    ;; ---powierline--------------------------------------------------
+    ;; --- powierline ------------------------------------------------
     '(powerline-active1 ((t (:inherit mode-line))))
     '(powerline-active2 ((t (:inherit mode-line))))
+
+    ;; ---- lsp ------------------------------------------------------
+    '(lsp-ui-doc-background ((t (:inherit mindre-subtle))))
+
+    ;; ---- rime -----------------------------------------------------
+    '(rime-indicator-dim-face ((t (:inherit mindre-faded))))
+    '(rime-indicator-face ((t (:inherit (mindre-keyword mindre-strong)))))
 
     ;; --- Racket ----------------------------------------------------
     `(racket-keyword-argument-face ((t (:inherit mindre-keyword)))))))
